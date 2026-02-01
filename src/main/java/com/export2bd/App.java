29713fa51688b2fbe0c6d3a7d86626d696a59537
Wilson Muñoz <wilson.mg.2047@gmail.com>
2026-01-31 19:40:39 -0600

    Initial frontend commit

---
+++ src/main/java/com/export2bd/App.java
@@ -0,0 +1,16 @@
+package com.export2bd;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    
+
+      @Override
+    public void start(Stage stage) {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
